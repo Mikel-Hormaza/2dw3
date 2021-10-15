96f774658d797af713cf2764bf7b7459d0befeb6
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-15 12:09:16 +0200

    (commit on 'Mikel')

--- Creacion de manuales.php
+++ Creacion de manuales.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+        <link rel="stylesheet" type="text/css" href="IS.css">
 </head>
 <body>
 
