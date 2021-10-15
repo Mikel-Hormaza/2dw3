1a743f1fb55ed313a6853e140c9008319f1562d4
Herrera04 <aitorherreraalves@gmail.com>
2021-10-15 11:33:25 +0200

    (commit on 'Aitor')

---
+++ IniciarSesion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fix Point</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+        <link rel="stylesheet" type="text/css" href="IS.css">   
+    </head>
+    <body>
+        <header>
+            <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="#">INICIO</a></li>
+                    <li><a href="#">BIBLIOTECA</a></li>
+                    <li><a href="#">ALQUILER</a></li>
+                    <li><a href="#">FIXPOINT</a></li>
+                </ul>
+            </nav>
+            <input type="button" value="Iniciar sesion" class="sesion">
+        </header>
+    </body>
+</html>
+
+<!--Cont&aacute;ctanos es para poner contactanos con tilde-->
