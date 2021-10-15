ff2d4819751c61a6a3607324c1cf18dc6a2fa03f
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-15 12:14:19 +0200

    (commit on 'Mikel')

--- Creacion de manuales.php
+++ Creacion de manuales.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+        <title>Fix Point</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="icon" type="image/png" href="Fotos/Icono.ico">
