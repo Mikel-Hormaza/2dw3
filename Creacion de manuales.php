0f812ff4382b7583033a485c6ece34972873414e
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-15 13:43:00 +0200

    (commit on 'Mikel')

--- Creacion de manuales.php
+++ Creacion de manuales.php
@@ -1,14 +1,45 @@
 <!DOCTYPE html>
 <html>
+
 <head>
         <title>Fix Point</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="icon" type="image/png" href="Fotos/Icono.ico">
-        <link rel="stylesheet" type="text/css" href="IS.css">
+        <link rel="stylesheet" type="text/css" href="Creacion de manuales.css">
 </head>
+
 <body>
+        <button>Volver a la gestion de manuales</button>
 
+        <input type="search" placeholder="Nombre de la herramienta">
 
+        <input type="text" placeholder="Nombre">
+
+        <textarea placeholder="Descripcion de la reparacion"></textarea>
+
+        <label>Imagen:</label>
+        <input type="file">
+
+        <textarea placeholder="Herramientas necesarias"></textarea>
+
+        <label>Medidas de seguridad:</label>
+        <textarea placeholder="Medidas de seguridad que hay  que tomar"></textarea>
+
+        <button>Guardar</button>
+
+        <div>
+                <h3>Pasos</h3>
+
+                <input type="text" placeholder="Titulo">
+
+                <textarea placeholder="Descripcion del paso"></textarea>
+
+                <label>Imagen:</label>
+                <input type="file">
+
+        </div>
+        <button>Guardar Paso</button>
 </body>
+
 </html>
