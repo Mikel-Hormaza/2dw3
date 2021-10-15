44bb14af200c72690e806792d6b089c3b8f1cfac
Herrera04 <aitorherreraalves@gmail.com>
2021-10-15 14:23:14 +0200

    (Confirmar en 'Aitor')

---
+++ Contactanos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fix Point</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+        <link rel="stylesheet" type="text/css" href="IS.css">   
+    </head>
+    <body>
+        <form action="" method=""></form>
+    </body>
+</html>
