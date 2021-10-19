d334ff9a18b6df07e90a41836c25ac6514bdea87
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-19 09:04:06 +0200

    (commit on 'Mikel')

---
+++ Inicio de sesion/Login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Fix Point</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+        <link rel="stylesheet" type="text/css" href="Creacion de manuales.css">
+</head>
+<body>
+<main>
+<img src="Fotos/Fix Point logo.PNG" width="500px" height="150px"><br>
+<label>Inicie sesion para acceder a contenido privado</label>
+</main>
+<form class="inicio">
+<input type="text" placeholder="Nombre de usuario"><br><br>
+<input type="text" placeholder="Contraseña"><br><br>
+<button>Iniciar sesion</button>
+<button>Crear nueva cuenta</button>
+</form>
+
+</body>
+</html>
