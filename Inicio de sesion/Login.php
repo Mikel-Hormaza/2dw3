bafc0e595e1cc4872217646571c340edcea59ae1
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-19 10:38:40 +0200

    (commit on 'Mikel')

--- Inicio de sesion/Login.php
+++ Inicio de sesion/Login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
         <title>Fix Point</title>
         <meta charset="utf-8">
@@ -7,17 +8,25 @@
         <link rel="icon" type="image/png" href="../Fotos/Icono.ico">
         <link rel="stylesheet" type="text/css" href="Login.css">
 </head>
-<body>
-<main>
-<img src="../Fotos/Fix Point logo.PNG" width="500px" height="150px"><br>
-<label>Inicie sesion para acceder a contenido privado</label>
-</main>
-<form class="inicio">
-<input type="text" placeholder="Nombre de usuario"><br><br>
-<input type="text" placeholder="Contraseña"><br><br>
-<button>Iniciar sesion</button>
-<button>Crear nueva cuenta</button>
-</form>
 
+<body>
+        <main class="iniciosesion">
+                <div class="logo">
+                        <img class="imagen" src="../Fotos/Fix Point logo.PNG" ><br>
+                        <label>Inicie sesion para acceder a contenido privado</label>
+                </div><br><br>
+
+                <fieldset class="borde">
+
+                        <form class="inicio">
+                                <input type="text" placeholder="Nombre de usuario"><br><br>
+                                <input type="text" placeholder="Contraseña"><br><br>
+                                <button class="botoninicio">Iniciar sesion</button><br><br>
+                                <button class="crearusuatio">Crear nueva cuenta</button>
+                        </form>                        
+                </fieldset>
+
+        </main>
 </body>
+
 </html>
