a0454ae312e6722a726acde5f379ce6400f99ce3
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-19 10:27:59 +0200

    (commit on 'Laura')

---
+++ visualizarManual/visualizarManual.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="visualizarManual.css">
+    <title>Visualizar manual</title>
+</head>
+
+<body>
+    <section>
+        <div id="amarilloMadre">
+            <img src="Imagenes/paso1.jpg" alt="Imagen paso">
+            <h1>Desmontaje del iPhone 12 Pro Max</h1>
+
+        </div>
+        <button class="botonVolver">Volver<span> a manuales</span></button>
+
+    </section>
+
+    <article>
+        <div id="infoManual">
+            <img src="Imagenes/paso1.jpg" alt="Imagen paso">
+            <h3>Código de manual: 1</h3>
+            <h3>Fecha creación: 20/10/2021</h3>
+            <h3>Creador: Laura Luengo</h3>
+            <p>
+                Hemos desmontado el mini iPhone y los dos iPhones medianos, pero hemos guardado el más grande para el final: el iPhone 12 Pro Max. Si viste nuestro livestream la semana pasada, pudiste ver lo que lleva, pero queríamos más (¡y nuestros mensajes directos nos dicen que tú también!)! Es hora de darle a este teléfono de gran cuerpo el tratamiento completo de desmontaje.
+
+                Hablando de dar: para poner fin a esta épica ola de desmontajes, regalamos a tres afortunados un Minnow Driver Kit! Aunque tendrás que trabajar para ello. Para poner a prueba la cámara del iPhone 12 Pro Max, la usamos para tomar en secreto dos fotos en este desmontaje con la Aplicación de cámara Halide. "Usa este formulario para adivinar correctamente qué dos pasos de abajo contienen una foto tomada en el iPhone, y entrarás para ganar.
+            </p>
+        </div>
+        <div>
+            <h4>Paso 1: </h4>
+            <p>Desmontaje del iPhone 12 Pro Max </p>
+            <img src="Imagenes/paso1.jpg" alt="">
+            <p>
+                En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+            </p>
+            <hr>
+        </div>
+        <div>
+            <h4>Paso 2: </h4>
+            <p>En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.</p>
+            <img src="Imagenes/paso2.jpg" alt="">
+            <p>
+                Los amigos no dejan que sus amigos desmonten los aparatos sin un plano de rayos X. Por suerte, somos los mejores amigos de Creative Electron que nos suministran un sinfín de rayos X.
+
+                Las partes básicas de este iPhone se ven casi igual que el resto de la familia del iPhone 12, pero el marco de acero oscuro es sólo del Pro. Además, ese anillo MagSafe que ni siquiera cabía en los 12 mini se ve muy mal en este Max.
+
+                Un artículo que nos interesa especialmente ver: la batería en forma de L más ancha... especialmente porque nos decepcionó el aburrido rectángulo del Pro. El Max solo se aferra a una L, pero, ¿los otros iPhones?
+            </p>
+            <hr>
+        </div>
+        <div>
+            <h4>Paso 3: </h4>
+            <p></p>
+            <img src="Imagenes/paso3.jpg" alt="">
+            <p>
+                Los amigos no dejan que sus amigos desmonten los aparatos sin un plano de rayos X. Por suerte, somos los mejores amigos de Creative Electron que nos suministran un sinfín de rayos X.
+
+                Las partes básicas de este iPhone se ven casi igual que el resto de la familia del iPhone 12, pero el marco de acero oscuro es sólo del Pro. Además, ese anillo MagSafe que ni siquiera cabía en los 12 mini se ve muy mal en este Max.
+
+                Un artículo que nos interesa especialmente ver: la batería en forma de L más ancha... especialmente porque nos decepcionó el aburrido rectángulo del Pro. El Max solo se aferra a una L, pero, ¿los otros iPhones?
+            </p>
+            <hr>
+        </div>
+    </article>
+</body>
+
+</html>
