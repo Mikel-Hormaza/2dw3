e2cb90c6a609be9f77931ce9f0098bca607a0547
Herrera04 <aitorherreraalves@gmail.com>
2021-10-19 14:22:04 +0200

    Confirmar en 'Aitor'

--- IniciarSesion.php
+++ IniciarSesion.php
@@ -1,53 +1,57 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Fix Point</title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="icon" type="image/png" href="Fotos/Icono.ico">
-        <link rel="stylesheet" type="text/css" href="IS.css">   
-    </head>
-    <body>
-        <header>
-            <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
-            <nav>
-                <ul>
-                    <li><a href="#">INICIO</a></li>
-                    <li><a href="#">BIBLIOTECA</a></li>
-                    <li><a href="#">ALQUILER</a></li>
-                    <li><a href="#">FIXPOINT</a></li>
-                </ul>
-            </nav>
-            <input type="button" value="Iniciar sesion" class="sesion">
-        </header>
-        <footer>
-            <div class="grid-container">
-                <div class="col1">
-                    <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
-                    <div>
-                        
-                        <p><img src="Fotos/Logo-telefono.png" alt="" class="telefono"> 666000999</p>
-                    </div>
-                    <div>
-                        
-                        <p><img src="Fotos/gmail.png" alt="" class="email"> fixpointt5@gmail.com</p>
-                    </div>
-                    <a href="#" class="contact">Cont&aacute;ctanos</a>
+
+<head>
+    <title>Fix Point</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+    <link rel="stylesheet" type="text/css" href="IS.css">
+</head>
+
+<body>
+    <header>
+        <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
+        <nav>
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">BIBLIOTECA</a></li>
+                <li><a href="#">ALQUILER</a></li>
+                <li><a href="#">FIXPOINT</a></li>
+            </ul>
+        </nav>
+        <input type="button" value="Iniciar sesion" class="sesion">
+    </header>
+    <footer>
+        <div class="grid-container">
+            <div class="col1">
+                <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
+                <div>
+
+                    <p><img src="Fotos/Logo-telefono.png" alt="" class="telefono"> 666000999</p>
                 </div>
-                <div class="redes" class="col2">
-                    <img src="Fotos/instagram.png" alt="" width="60px" height="60px">
-                    <img src="Fotos/Twitter.png" alt="" class="red">
-                    <img src="Fotos/Facebook.png" alt="" class="red">
+                <div>
+
+                    <p><img src="Fotos/gmail.png" alt="" class="email"> fixpointt5@gmail.com</p>
                 </div>
-                <div class="col3">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2975.868228083871!2d-2.484367449034827!3d41.766498779129286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd44d2e709876957%3A0x469c9525026cc4ad!2sCentro%20Integrado%20De%20Formaci%C3%B3n%20Profesional%20Pic%C3%B3%20Frentes!5e0!3m2!1ses!2ses!4v1634299318239!5m2!1ses!2ses" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                    <div>
-                        <p><img src="Fotos/maps.png" alt="" class="url">ppppppppppppppppppppppppppppppppppp</p>
-                    </div>
+                <br><br>
+                <a href="#" class="contact">Cont&aacute;ctanos</a>
+            </div>
+            <div class="redes" class="col2">
+                <img src="Fotos/instagram.png" alt="" width="60px" height="60px">
+                <img src="Fotos/Twitter.png" alt="" class="red">
+                <img src="Fotos/Facebook.png" alt="" class="red">
+            </div>
+            <div class="col3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2975.868228083871!2d-2.484367449034827!3d41.766498779129286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd44d2e709876957%3A0x469c9525026cc4ad!2sCentro%20Integrado%20De%20Formaci%C3%B3n%20Profesional%20Pic%C3%B3%20Frentes!5e0!3m2!1ses!2ses!4v1634299318239!5m2!1ses!2ses" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <div>
+                    <p id="textomedio"><img src="Fotos/maps.png" alt="" class="url">ppppppppppppppppppppppppppppppppppp</p>
                 </div>
             </div>
-        </footer>
-    </body>
+        </div>
+    </footer>
+</body>
+
 </html>
 
 <!--Cont&aacute;ctanos es para poner contactanos con tilde-->
