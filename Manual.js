121efe4da5f017a9b2e37769aafc7918176d539e
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-21 12:42:29 +0200

    (commit on 'Laura')

---
+++ Manual.js
@@ -0,0 +1,19 @@
+class Manual {
+  constructor(p_nombreManual,
+    p_informacionManual,
+    p_equipoNecesario,
+    p_medidasDeSeguridad,
+    p_medidasDeSeguridad,
+    p_fotoManual,
+    p_codHerramienta,
+    p_codUsuario,
+    p_fechaCreacion) {
+    this.informacionManual = p_informacionManual;
+    this.equipoNecesario = p_equipoNecesario;
+    this.medidasDeSeguridad = p_medidasDeSeguridad;
+    this.fotoManual = p_fotoManual;
+    this.codHerramienta = p_codHerramienta;
+    this.codUsuario = p_codUsuario;
+    this.fechaCreacion = p_fechaCreacion;    
+  }
+}
