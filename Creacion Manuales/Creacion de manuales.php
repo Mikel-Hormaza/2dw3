1cf3533af7cb3d2f01b6368e34e524a55bded543
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-21 13:22:21 +0200

    (Finalizacion de los cambios)

--- Creacion Manuales/Creacion de manuales.php
+++ Creacion Manuales/Creacion de manuales.php
@@ -11,46 +11,105 @@
 
 <body>
         <!--Contenedor del boton de regreso el buscador de herrramientas y el fromulario de manuales-->
-        <main class="contenedor">
-                <ul class="burua">
-                        <button class="return">Volver a la gestion de manuales</button>
 
-                        <input class="buscador" type="search" placeholder="Nombre de la herramienta">
-                </ul>
+        <div class="burua">
+                <button class="return">Volver a la gestion de manuales</button>
 
-                <ul class="formulario">
-                        <input type="text" placeholder="Nombre"><br>
-
-                        <textarea placeholder="Descripcion de la reparacion"></textarea><br>
-
-                        <label>Imagen:</label>
-                        <input type="file"><br>
-
-                        <textarea placeholder="Herramientas necesarias"></textarea><br>
-
-                        <textarea placeholder="Medidas de seguridad que hay  que tomar"></textarea><br>
-
-                        <button class="gorde">Guardar</button>
-                </ul>
-        </main>
-
-        <div class="pasos">
-                <form class="fondo">
-                        <h3>Pasos</h3>
-
-                        <input type="text" placeholder="Titulo"><br>
-
-                        <textarea placeholder="Descripcion del paso"></textarea><br>
-
-                        <label class="labelimg">Imagen:</label>
-                        <input class="imagen" type="file"><br>
-
-                </form><br>
-        </div><br>
-
-        <div class="boton">
-                <button class="gordepauso">Guardar Paso</button>
+                <input class="buscador" type="search" placeholder="Nombre de la herramienta">
         </div>
+        <main class="contenedor">
+                <div class="formularios">
+                        <form class="formulario">
+                                <input type="text" placeholder="Nombre"><br>
+
+                                <textarea placeholder="Descripcion de la reparacion"></textarea><br>
+
+                                <label>Imagen:</label>
+                                <input type="file"><br>
+
+                                <textarea placeholder="Herramientas necesarias"></textarea><br>
+
+                                <textarea placeholder="Medidas de seguridad que hay  que tomar"></textarea><br>
+
+                                <button class="gorde">Guardar</button>
+</form>
+
+
+
+
+                        <form class="fondo">
+                                <h4>Pasos</h4>
+
+                                <input type="text" placeholder="Titulo"><br>
+
+                                <textarea placeholder="Descripcion del paso"></textarea><br>
+
+                                <label class="labelimg">Imagen:</label>
+                                <input class="imagen" type="file"><br>
+
+
+                                <button class="gordepauso">Guardar Paso</button>
+
+                        </form><br>
+
+
+                </div>
+
+
+                <div class="mostrarpasos">
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Fotos/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Fotos/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Fotos/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                </div>
+        </main>
 </body>
 
 </html>
