bbea0c3e1acddc2ffa2cc20f87dbb5779c135f46
Herrera04 <aitorherreraalves@gmail.com>
2021-10-21 10:21:52 +0200

    confirmar en 'Aitor'

--- Contactanos.php
+++ Contactanos.php
@@ -5,9 +5,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="icon" type="image/png" href="Fotos/Icono.ico">
-        <link rel="stylesheet" type="text/css" href="Contacto.css">   
+        <link rel="stylesheet" type="text/css" href="Contacto.css">  
+        <link rel="stylesheet" type="text/css" href="Header.css">   
     </head>
     <body>
+        <?php require_once 'Header.php' ?>
         <form action="" method="">
             <fieldset>
                 <legend>Formulario</legend>
@@ -17,8 +19,7 @@
                 <input type="text" name="izena" placeholder=""><br>
                 <textarea name="" id="" cols="30" rows="10"></textarea>
             </fieldset>
-               
-            
         </form>
+        <?php require_once 'footer.php' ?>
     </body>
 </html>
