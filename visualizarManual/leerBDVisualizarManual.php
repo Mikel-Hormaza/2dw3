1da284d0ae8db5a20a009d4281ad10c8d22a480c
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-22 13:57:09 +0200

    (commit on 'Laura')

--- visualizarManual/leerBDVisualizarManual.php
+++ visualizarManual/leerBDVisualizarManual.php
@@ -1,16 +1,17 @@
 <?php
 $servidor  = "localhost";
-$usuario= "root";
-$password="";
+$usuario = "root";
+$password = "";
 
 /* <!-- 
 AQUÍ PASAR COMO SESSION EL COD DE MANUAL?
  --> */
- $_SESSION['codManualSeleccionado']=2;
- $codManualSeleccionado=$_SESSION['codManualSeleccionado'];
+$_SESSION['codManualSeleccionado'] = 2;
+$codManualSeleccionado = $_SESSION['codManualSeleccionado'];
 
-try{
-    $conexion = new PDO("mysql:host=$servidor;dbname=fixpoint",$usuario, $password);
+/*Conectar con la BD y leer los datos del manual y sus pasos*/
+try {
+    $conexion = new PDO("mysql:host=$servidor;dbname=fixpoint", $usuario, $password);
 
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -19,18 +20,75 @@ try{
     WHERE manual.codManual like $codManualSeleccionado
     && manual.codUsuario like usuario.codUsuario";
 
-    $sqlPasos="SELECT tituloPaso, descripcionPaso, fotoPaso 
+    $sqlPasos = "SELECT tituloPaso, descripcionPaso, fotoPaso 
     FROM paso
     WHERE codManual like $codManualSeleccionado";
-    
-    $resultadoManual=$conexion->query($sqlManual);
+
+    $resultadoManual = $conexion->query($sqlManual);
     $datosManual = $resultadoManual->fetchAll();
 
-    $resultadoPasos=$conexion->query($sqlPasos);
+    $resultadoPasos = $conexion->query($sqlPasos);
     $datosPasos = $resultadoPasos->fetchAll();
-
-}catch(PDOException $e){
-    echo $sqlManual. "<br>" . $e->getMessage();
+} catch (PDOException $e) {
+    echo $sqlManual . "<br>" . $e->getMessage();
 }
 
-$conexion=null;
+$conexion = null;
+
+$largo = strlen($datosManual[0]["informacionManual"]);
+$informacionManual = $datosManual[0]["informacionManual"];
+$equipoNecesario = $datosManual[0]["equipoNecesario"];
+$medidasDeSeguridad = $datosManual[0]["medidasDeSeguridad"];
+
+function mostrarBotonMostrar($texto, $tipoDeDato)
+{
+    global $largo;
+    if ($largo > 200) {
+        textoCortado($texto, $tipoDeDato);
+    } else {
+        textoEntero($tipoDeDato);
+    }
+}
+
+function textoEntero($texto)
+{ ?>
+    <p><?php $texto ?></p>
+    <?php
+}
+
+function textoCortado($texto, $tipoDeDato)
+{
+    /*cortar a los X caracteres en vez de con un punto porque si no hay punto...*/
+    $primero = stristr($texto, ". ", 1);
+    $segundo = stristr($texto, ". ", 0);
+
+    switch ($tipoDeDato) {
+        case 1:
+    ?>
+            <p><?php echo $primero ?>
+                <span id="puntos">...</span><span id="mas"><?php echo $segundo ?></span>
+                <br>
+                <button id="botonLeerMas">Mostrar</button>
+            </p><?php
+                break;
+            case 2:
+                ?>
+            <p><?php echo $primero ?>
+                <span id="puntosEquipo">...</span><span id="masEquipo"><?php echo $segundo ?></span>
+                <br>
+                <button id="botonLeerMasEquipo">Mostrar</button>
+            </p>
+        <?php
+                break;
+            case 3:
+        ?>
+            <p><?php echo $primero ?>
+                <span id="puntosSeguridad">...</span><span id="masSeguridad"><?php echo $segundo ?>
+                </span> <br><button id="botonLeerMasSeguridad">Mostrar</button>
+            </p>
+<?php
+                break;
+        }
+    }
+
+?>
--- visualizarManual/visualizarManual.php
+++ visualizarManual/visualizarManual.php
@@ -22,35 +22,25 @@
     <article>
         <div id="informacionInicial">
             <div id="infoManual">
-            <img src="Imagenes/<?php echo $paso["fotoManual"] ?>" alt="">
+            <img src="Imagenes/<?php echo $datosManual[0]["fotoManual"] ?>" alt="">
                 <div>
                     <h3>Código de manual: <?php echo $datosManual[0]["codManual"]; ?></h3>
                     <h3>Fecha creación: <?php echo $datosManual[0]["fechaCreacion"]; ?></h3>
                     <h3>Creador: <?php echo $datosManual[0]["nomUsuario"]; ?></h3>
                 </div>
-                <p>
-                    Hemos desmontado el mini iPhone y los dos iPhones medianos, pero hemos guardado el más grande para el final: el iPhone 12 Pro Max. <span id="puntos">...</span><span id="mas"> Si viste nuestro livestream la semana pasada, pudiste ver lo que lleva, pero queríamos más (¡y nuestros mensajes directos nos dicen que tú también!)! Es hora de darle a este teléfono de gran cuerpo el tratamiento completo de desmontaje.
-                        Hablando de dar: para poner fin a esta épica ola de desmontajes, regalamos a tres afortunados un Minnow Driver Kit! Aunque tendrás que trabajar para ello. Para poner a prueba la cámara del iPhone 12 Pro Max, la usamos para tomar en secreto dos fotos en este desmontaje con la Aplicación de cámara Halide. "Usa este formulario para adivinar correctamente qué dos pasos de abajo contienen una foto tomada en el iPhone, y entrarás para ganar.</span> <br><button id="botonLeerMas">Mostrar</button></p>
+                <?php mostrarBotonMostrar($informacionManual, 1);?>
             </div>
             <div id="equipoNecesario">
                 <h4>Equipo necesario</h4>
                 <div>
-                    <p>
-                        Llave Allen. Lo ideal es contar con un kit que incorpore diferentes medidas.<span id="puntosEquipo">...</span><span id="masEquipo"> Muchos muebles en kit las traen incorporadas para el montaje.
-
-                            Linterna. Para acceder a espacios recónditos que necesitan reparación. Mejor tenerla a mano por si hay algún apagón en casa…
-
-                            Cúter. Esencial para hacer cortes limpios sin peligro… Precio: 4 euros.</span> <br><button id="botonLeerMasEquipo">Mostrar</button>
-                    </p>
+                <?php mostrarBotonMostrar($equipoNecesario, 2);?>
                 </div>
             </div>
             <div id="seguridad">
                 <h4>Medidas de seguridad</h4>
                 <div>
-                    <p>
-                        Asegúrate de seguir el canal de iFixit YouTube,. nuestro Instagram, y nuestro <span id="puntosSeguridad">...</span><span id="masSeguridad"> Twitter, y suscríbete a nuestro boletín de noticiaspara que seas el primero en saber cuando la nueva tecnología de consumo llega a la mesa de desmontaje.
-                        </span> <br><button id="botonLeerMasSeguridad">Mostrar</button></p>
-                </div>
+                <?php mostrarBotonMostrar($medidasDeSeguridad, 3);?>
+            </div>
             </div>
         </div>
 
