20ca91def5dbfaba55557e680137a29c2858a2a2
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-22 10:32:18 +0200

    (Header y footer)

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Fix Point</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="Fotos/Icono.ico">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+</head>
+
+<body>
+    <footer class="footer-distributed">
+        <div class="footer-I">
+            <img src="Fotos/Fix Point logo.PNG" alt="" class="logo">
+            <p>FixPoint</p>
+            <p>Manuales y alquileres de herramientas</p>
+            <a href="#" class="contact">Cont&aacute;ctanos</a>
+        </div>
+        <div class="footer-C">
+            <div class="Footermain">
+                <img src="Fotos/maps.png" alt="" class="maps">
+                <p><span>Calle Gervasio Manrique de Lara</span> Soria, España</p>
+            </div>
+            <br>
+            <div class="Footermain">
+                <img src="Fotos/Logo-telefono.png" alt="" class="telefono">
+                <p>+34 975 23 94 43</p>
+            </div>
+            <br>
+            <div class="Footermain">
+                <img src="Fotos/gmail.png" alt="" class="gmail">
+                <p>fixpointt5@gmail.com</p>
+            </div>
+        </div>
+        <div class="footer-D">
+            <p class="footer-logos">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2975.868228083871!2d-2.484367449034827!3d41.766498779129286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd44d2e709876957%3A0x469c9525026cc4ad!2sCentro%20Integrado%20De%20Formaci%C3%B3n%20Profesional%20Pic%C3%B3%20Frentes!5e0!3m2!1ses!2ses!4v1634299318239!5m2!1ses!2ses" width="220" height="120" style="border:0;" frameborder=”0” allowfullscreen="" loading="lazy" aria-hidden="false" tabindex="0"></iframe>
+            </p>
+            <div class="footer-icons">
+                <a href="#"><img src="Fotos/facebook-f.png" alt="" class="facebook"></a>
+                <a href="#"><img src="Fotos/instagram-f.png" alt="" class="instagram"></a>
+                <a href="#"><img src="Fotos/twitter-f.png" alt="" class="instagram"></a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
