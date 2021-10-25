52554ed609656922840af8b4b9d0461dfa8f1dc0
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-25 14:22:40 +0200

    (commit on Mikel)

--- gestionHerramientas/leerBDGestionManual.php
+++ gestionHerramientas/leerBDGestionManual.php
@@ -9,12 +9,14 @@ try {
     $konexioa = new PDO("mysql:host=$servidor;dbname=fixpoint",$usuario, $password);
     $konexioa->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sqlherramienta="SELECT nombreHerramienta, categoria, fotoHerramienta 
+    $sqlherramienta="SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
     FROM herramienta";
 
 $resultadoHerramienta = $conexion->query($sqlherramienta);
 $datosHerramienta = $resultadoHerramienta->fetchAll();
 
+$sqlherramienta="SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
+    FROM herramienta";
 
 }catch (PDOException $e){
     echo $sqlherramienta."<br>" .$e->getMessage();
