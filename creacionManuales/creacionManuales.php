4d5eedb4d0a8a01884d77eb28751307886aa33a9
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-25 09:39:09 +0200

    (commit on  'Laura')

---
+++ creacionManuales/creacionManuales.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <title>Fix Point</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/png" href="../Imagenes/Icono.ico">
+        <link rel="stylesheet" type="text/css" href="creacionManuales.css">
+</head>
+
+<body>
+<?php require_once '../Header.php'?>
+        <!--Contenedor del boton de regreso el buscador de herrramientas y el fromulario de manuales-->
+
+        <div class="burua">
+                <button class="return">Volver a la gestion de manuales</button>
+
+                <input class="buscador" type="search" placeholder="Nombre de la herramienta">
+        </div>
+        <main class="contenedor">
+                <div class="formularios">
+                        <form class="formulario">
+                                <input type="text" placeholder="Nombre"><br>
+
+                                <textarea placeholder="Descripcion de la reparación"></textarea><br>
+
+                                <label>Imagen:</label>
+                                <input type="file"><br>
+
+                                <textarea placeholder="Herramientas necesarias"></textarea><br>
+
+                                <textarea placeholder="Medidas de seguridad que hay que tomar"></textarea><br>
+
+                                <button class="gorde">Guardar</button>
+                        </form>
+
+                        <form class="fondo">
+                                <h4>Pasos</h4>
+
+                                <input type="text" placeholder="Titulo"><br>
+
+                                <textarea placeholder="Descripción del paso"></textarea><br>
+
+                                <label class="labelimg">Imagen:</label>
+                                <input class="imagen" type="file"><br>
+
+
+                                <button class="gordepauso">Guardar Paso</button>
+
+                        </form><br>
+
+
+                </div>
+
+
+                <div class="mostrarpasos">
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Imagenes/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Imagenes/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                        <div class="paso">
+                                <div>
+                                        <h3 class="pasoTitulo">Paso 1 </h3>
+                                        <p>Desmontaje del iPhone 12 Pro Max </p>
+                                </div>
+                                <img src="Imagenes/reparacion.jpg" alt="">
+                                <p>
+                                        En caso de que no lo hayas oído, el cuadrado es redondo... de nuevo.
+
+                                        Siento decírtelo, pero el tamaño importa, al menos en lo que se refiere a los sensores de la cámara. Para lograr su supuesta mejora del 87% en el rendimiento de la luz baja, el 12 Pro Max utiliza el mayor sensor de un iPhone hasta ahora. Pero antes de que nos pongamos nerviosos con las cámaras, comparemos los iPhones con los iPhones:
+
+                                        En nuestra línea de tiempo del iPhone tenemos un iPhone 11 Pro Max verde, un iPhone 12 Pro dorado, y un iPhone 12 Pro Max azul.
+
+                                        Si lo tuyo son las cosas grandes, el iPhone 12 Pro Max es un sueño. Todo es más grande, incluso el bulto de la cámara.
+                                </p>
+                                <hr>
+                        </div>
+                </div>
+        </main>
+
+        <?php require_once '../footer.php'?>
+
+</body>
+
+</html>
