3925edc706cc99b9a398e72e613bf97fe0bc23a4
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-26 11:51:28 +0200

    (commit on 'Laura')

--- visualizarManual/visualizarManual.js
+++ visualizarManual/visualizarManual.js
@@ -1,44 +1,74 @@
+let div1;
+let div2;
+let div3;
+
 window.addEventListener("load", inicio);
 
 /*Mostrar más*/
 function inicio() {
-  calcularSiAnadirFuncionAlBoton();
-/*     document.getElementById("botonLeerMas").addEventListener("click", mostrarDetalles);
-    document.getElementById("botonLeerMasEquipo").addEventListener("click", mostrarEquipo);
-    document.getElementById("botonLeerMasSeguridad").addEventListener("click", mostrarSeguridad); */
+  obtenerDivs();
 }
 
-/*Comprobar si hay uno o dos párrafos een los div. Si hay uno, no añadir addEventListener al botón porque no existe.
-Si hay dos, añadir*/
-function calcularSiAnadirFuncionAlBoton(){
-  let div1 = document.getElementById("divInfoDetallesManual");
-  let largoDeDiv1 = div1.getElementsByTagName("P");
-  alert(largoDeDiv1.length);
-}  
+/*Obtener los divs con párrafos*/
+function obtenerDivs() {
+  div1 = document.getElementById("divInfoDetallesManual");
+  div2 = document.getElementById("divInfoEquipoNecesario");
+  div3 = document.getElementById("divInfoMedidasSeguridad");
+  comprobarNumeroDeParrafos(div1, 1);
+  comprobarNumeroDeParrafos(div2, 2);
+  comprobarNumeroDeParrafos(div3, 3);
+}
 
-function mostrarDetalles(){
+/*Comprobar si hay uno o dos párrafos een los div. 
+Si hay uno, no añadir addEventListener al botón porque no existe.
+Si hay dos, añadir*/
+function comprobarNumeroDeParrafos(div, tipo) {
+  let elementosDiv = div.getElementsByTagName("P");
+  if (elementosDiv > 1) {
+    anadirEventListener(tipo);
+  }
+}
+
+/*Añadir eventListeners segun tipo de dato (detales, equipo, seguridad)*/
+function anadirEventListener(tipo) {
+  switch (tipo) {
+    case 1:
+      document.getElementById("botonLeerMas").addEventListener("click", mostrarDetalles);
+      break;
+    case 2:
+      document.getElementById("botonLeerMasEquipo").addEventListener("click", mostrarEquipo);
+      break;
+    case 3:
+      document.getElementById("botonLeerMasSeguridad").addEventListener("click", mostrarSeguridad);
+      break;
+  }
+}
+
+function mostrarDetalles() {
   mostrarTexto("puntos", "mas", "botonLeerMas");
 }
-function mostrarEquipo(){
+
+function mostrarEquipo() {
   mostrarTexto("puntosEquipo", "masEquipo", "botonLeerMasEquipo");
 }
-function mostrarSeguridad(){
+
+function mostrarSeguridad() {
   mostrarTexto("puntosSeguridad", "masSeguridad", "botonLeerMasSeguridad");
 }
 
 /*Mostar la parte del texto oculta*/
 function mostrarTexto(puntosSuspensivos, mas, boton) {
-    let puntos = document.getElementById(puntosSuspensivos);
-    let masText = document.getElementById(mas);
-    let btnText = document.getElementById(boton);
-  
-    if (puntos.style.display == "none") {
-      puntos.style.display = "inline";
-      btnText.innerHTML = "Mostrar"; 
-      masText.style.display = "none";
-    } else {
-      puntos.style.display = "none";
-      btnText.innerHTML = "Ocultar"; 
-      masText.style.display = "inline";
-    }
+  let puntos = document.getElementById(puntosSuspensivos);
+  let masText = document.getElementById(mas);
+  let btnText = document.getElementById(boton);
+
+  if (puntos.style.display == "none") {
+    puntos.style.display = "inline";
+    btnText.innerHTML = "Mostrar";
+    masText.style.display = "none";
+  } else {
+    puntos.style.display = "none";
+    btnText.innerHTML = "Ocultar";
+    masText.style.display = "inline";
   }
+}
