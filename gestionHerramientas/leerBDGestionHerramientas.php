eaa7e18096cc8d40d08d24d91212b8a0fc57c689
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-10-26 09:39:55 +0200

    (Cambios)

--- gestionHerramientas/leerBDGestionHerramientas.php
+++ gestionHerramientas/leerBDGestionHerramientas.php
@@ -6,20 +6,23 @@ $password = "";
 
 
 try {
-    $conexion = new PDO("mysql:host=$servidor;dbname=fixpoint",$usuario, $password);
+    $conexion = new PDO("mysql:host=$servidor;dbname=fixpoint", $usuario, $password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sqlherramienta="SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
+    $sqlherramienta = "SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
     FROM herramienta";
 
     $resultadoHerramienta = $conexion->query($sqlherramienta);
     $datosHerramienta = $resultadoHerramienta->fetchAll();
 
-    $sqlherramienta="SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
+    $sqlherramientaususario = "SELECT codHerramienta, nombreHerramienta, categoria, fotoHerramienta 
     FROM herramienta, manual, usuario WHERE herramienta.codHerramienta=manual.codHerramienta && manual.codUsuario=usuario.codUsuario";
 
-}catch (PDOException $e){
-    echo $sqlherramienta."<br>" .$e->getMessage();
+    $resultadoHerramienta = $conexion->query($sqlherramientaususario);
+    $datosHerramienta = $resultadoHerramienta->fetchAll();
 
+
+} catch (PDOException $e) {
+    echo $sqlherramienta . "<br>" . $e->getMessage();
 }
-$konexioa=null;
+$konexioa = null;
