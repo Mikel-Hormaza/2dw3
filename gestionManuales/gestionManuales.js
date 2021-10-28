b7c4ca339f39d6779a87c2bd01a7ae1afce8b769
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-10-28 14:12:56 +0200

    (commit on 'Laura')

--- gestionManuales/gestionManuales.js
+++ gestionManuales/gestionManuales.js
@@ -1,38 +1,81 @@
 window.addEventListener("load", inicio);
-/*Si el usuario clica cuando el desplegable está desplegado, ocultarlo*/
-window.addEventListener("click", ocultarBloqueDesplegado);
 
-/*Clicar idBotonDesplegar: desplegarContenido
-categoria: desplegarBotonCategoria*/
 function inicio() {
-    document.getElementById("idBotonDesplegar").addEventListener("click", desplegarContenido);
-    document.getElementById("categoria").addEventListener("click", desplegarBotonCategoria);
-    document.getElementById("idBotonCategoria").addEventListener("click", desplegarContenidoCategoria);
+    document.getElementById("idBotonDesplegar").addEventListener("click", desplegable1);
+    document.getElementById("categoria").addEventListener("click", desplegable2);
+    document.getElementById("idBotonCategoria").addEventListener("click", desplegable3);
 }
 
-
-
-/* Al clicar el botón, añadir al elemento la clase mostrarDesplegarOpciones con display block*/
-/* function desplegarContenido() {
-    document.getElementById("idBloqueDesplegar").classList.toggle("mostrarDesplegarOpciones");
-}
-function desplegarBotonCategoria(){
-    document.getElementById("idBotonCategoria").classList.toggle("mostrarDesplegarOpciones");
+function desplegable1(){
+ comprobarSiElBloqueTieneClaseMostrar ("idBloqueDesplegar");
 }
 
-function desplegarContenidoCategoria(){
-    document.getElementById("idContenidoCategoria").classList.toggle("mostrarDesplegarOpciones");
+function desplegable2(){
+ comprobarSiElBloqueTieneClaseMostrar ("idBotonCategoria");
 }
 
-function ocultarBloqueDesplegado(){
-    if (!event.target.matches('.botonDesplegar')) {
-        let contenido = document.getElementsByClassName("contenidoADesplegar");
-        let i;
-        for (i = 0; i < contenido.length; i++) {
-            let abrirContenido = contenido[i];
-            if (abrirContenido.classList.contains('mostrarDesplegarOpciones')) {
-                abrirContenido.classList.remove('mostrarDesplegarOpciones');
-            }
+function desplegable3(){
+ comprobarSiElBloqueTieneClaseMostrar ("idContenidoCategoria");
+}
+
+/*Si divPlegable True-llamar a ocultar
+ELSE llamar a mostrar*/
+function comprobarSiElBloqueTieneClaseMostrar (idBloque) {
+    let divPlegable;
+    if (idBloque == "idBloqueDesplegar") {
+        divPlegable = document.getElementById(idBloque).classList.contains("mostrarDesplegarOpciones");
+    }else if(idBloque == "idBotonCategoria"){
+        divPlegable = document.getElementById(idBloque).classList.contains("mostrarBotonCategoria");
+    }else if (idBloque=="idContenidoCategoria"){
+        divPlegable = document.getElementById(idBloque).classList.contains("mostrarDesplegarCategorias");
+    }
+    if (divPlegable){
+        ocultarBloque(idBloque);
+    }else{
+        mostrarBloque(idBloque);
+    }
+}
+
+/*La primer vez que mostremos un bloque éste no tiene clase "ocultar",
+para comprobar si hace falta quitarlo llamamos a comprobarSiElBloqueTieneClaseOcultar */
+function mostrarBloque(idBloque){
+    comprobarSiElBloqueTieneClaseOcultar (idBloque);
+    if (idBloque == "idBloqueDesplegar") {
+        document.getElementById(idBloque).classList.add("mostrarDesplegarOpciones");
+    }else if(idBloque == "idBotonCategoria"){
+        document.getElementById(idBloque).classList.add("mostrarBotonCategoria");
+    }else if (idBloque=="idContenidoCategoria"){
+        document.getElementById(idBloque).classList.add("mostrarDesplegarCategorias");
+    }
+}
+
+/*Quitar la clase "mostrar" y añadir ocultar*/
+function ocultarBloque(idBloque){
+    if (idBloque == "idBloqueDesplegar") {
+        document.getElementById(idBloque).classList.remove("mostrarDesplegarOpciones");
+        document.getElementById(idBloque).classList.add("ocultarDesplegarOpciones");
+    }else if(idBloque == "idBotonCategoria"){
+        document.getElementById(idBloque).classList.remove("mostrarBotonCategoria");
+        document.getElementById(idBloque).classList.add("ocultarDesplegarCategorias");
+    }else if (idBloque=="idContenidoCategoria"){
+        document.getElementById(idBloque).classList.remove("mostrarDesplegarCategorias");
+        document.getElementById(idBloque).classList.add("ocultarBotonCategoria");
+    }
+}
+/*Si un elemento tiene la clase ocultar, quitársela*/
+function comprobarSiElBloqueTieneClaseOcultar (idBloque) {
+    if (idBloque == "idBloqueDesplegar") {
+        if(document.getElementById(idBloque).classList.contains("ocultarDesplegarOpciones")){
+            document.getElementById(idBloque).classList.remove("ocultarDesplegarOpciones");
+        }
+    }else if(idBloque == "idBotonCategoria"){
+        if(document.getElementById(idBloque).classList.contains("ocultarDesplegarCategorias")){
+            document.getElementById(idBloque).classList.remove("ocultarDesplegarCategorias");
+        }
+    }else if (idBloque=="idContenidoCategoria"){
+        if(document.getElementById(idBloque).classList.contains("ocultarBotonCategoria")){
+            document.getElementById(idBloque).classList.remove("ocultarBotonCategoria");
         }
     }
-} */
+}
+
