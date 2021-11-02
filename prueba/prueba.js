a25366463469edfca5b10abbb429ef6479a0611b
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-02 14:20:54 +0100

    (aún no borrra)

--- prueba/prueba.js
+++ prueba/prueba.js
@@ -1,3 +1,5 @@
+//declaro el array
+
 let arrayOpciones = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"];
 
 window.addEventListener("load", inicio);
@@ -5,14 +7,47 @@ window.addEventListener("load", inicio);
 
 
 function inicio() {
-    document.getElementById("idBusquedaNombreHerramienta").addEventListener("keyup", filtra);
-
+    //después de meter cada caracter
+    document.getElementById("idBusquedaNombreHerramienta").addEventListener("keyup", comprobarSiHayElementos);
 }
 
-function filtra(){
-    for(i=0; i<arrayOpciones.length; i++){
-        if(arrayOpciones[i].includes(document.getElementById("idBusquedaNombreHerramienta").value)){
-            console.log(arrayOpciones[i]);
+function filtra() {
+    let elementoCoincide;
+    let parrafo;
+    if (document.getElementById("idBusquedaNombreHerramienta").value.length > 0) {
+        //recorro el array y si algún string CONTIENE el texto, lo muestro
+        for (i = 0; i < arrayOpciones.length; i++) {
+            if (arrayOpciones[i].includes(document.getElementById("idBusquedaNombreHerramienta").value)) {
+                let elementoLista = elementoCoincide += i;
+                let p = parrafo += i;
+                //elemento de lista
+                elementoLista = document.createElement("li");
+                elementoLista.setAttribute("class", "elementoLista");
+                //parrafo del elemento de lista
+                p = document.createElement("p");
+                p.setAttribute("class", "parrafoElementoLista");
+                p = document.createTextNode(arrayOpciones[i]);
+                elementoLista.appendChild(p);
+                document.getElementById("mostrarBloqueResultados").appendChild(elementoLista);
+            }
         }
     }
+}
+
+function comprobarSiHayElementos() {
+    let elementosLista = document.getElementById("mostrarBloqueResultados").childElementCount;
+    if (elementosLista > 0) {
+        alert("HAYQUEelimunar");
+        eliminarElementosBusquedaPrevia();
+        //filtra();
+    } else {
+        filtra();
+    }
+}
+
+function eliminarElementosBusquedaPrevia() {
+        let elementoAEliminar = ul.getElementsByClassName("elementoLista");
+        elementoAEliminar.remove();
+        let parrafoElementoAEliminar = ul.getElementsByClassName("parrafoElementoLista");
+        parrafoElementoAEliminar.remove();
 }
