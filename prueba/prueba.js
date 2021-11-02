a796415206acefea2367d70c33c0718c87b9a1da
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-02 12:56:41 +0100

    (commit on Laura)

---
+++ prueba/prueba.js
@@ -0,0 +1,5 @@
+let arrayOpciones = ["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"];
+
+for(i=0; 0<arrayOpciones.length; i++){
+    console.log(arrayOpciones[i]);
+}
---
+++ prueba/prueba.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <script src="prueba.js"></script>
+</head>
+
+<body>
+
+    <div id="contenedor">
+        <form id="formulario" action="#" method="POST" onsubmit="false">
+            <input id="idBusquedaNombreHerramienta" type="text" name="herramienta" placeholder="Type to search..">
+        </form>
+
+        <ul id="mostrarBloqueResultados">
+        </ul>
+    </div>
+</body>
+
+</html>
