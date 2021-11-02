c63a634b0249c9d4e08da1732785427857d619e8
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-02 08:45:45 +0100

    (commit on 'Laura')

---
+++ prueba/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Autocomplete Search Box | CodingNepal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="prueba.js"></script>
+  </head>
+  <body>
+
+      <div id="contenedor">
+        <input id="idBusquedaNombreHerramienta" type="text" placeholder="Type to search..">
+        <div id="mostrarBloqueResultados">
+
+        </div>
+      </div>
+
+
+
+  </body>
+</html>
