30c67e2da04191c68d80e8882e86f0056e7769c2
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-02 08:45:10 +0100

    commit on 'Laura'

---
+++ prueba/prueba.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", inicio);
+
+function inicio() {
+    document.getElementById("idBotonDesplegar").addEventListener("click", desplegable1);
+    document.getElementById("categoria").addEventListener("click", desplegable2);
+    document.getElementById("idBotonCategoria").addEventListener("click", desplegable3);
+}
