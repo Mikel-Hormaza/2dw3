66cd870804e73408c21fc46569f134aad0ef1ed7
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-03 18:18:35 +0100

    (commit on 'Laura')

--- prueba/buscadorHerramientas.js
+++ prueba/buscadorHerramientas.js
@@ -1,11 +1,11 @@
 //declaro el array
 
-let arrayOpciones=[];
-
+let arrayOpciones = [];
+let contador;
 window.addEventListener("load", inicio);
 
 function inicio() {
-    arrayOpciones =leerPHP(); //lee los nombres de herramientas
+    arrayOpciones = leerPHP(); //lee los nombres de herramientas
     document.getElementById("idBusquedaNombreHerramienta").addEventListener("keyup", comprobarSiHayElementos);
 }
 
@@ -24,6 +24,7 @@ function comprobarSiHayElementos() {
 de los elementos del array. Muestra las concidencias como elementos de una lista */
 function filtra() {
     if (document.getElementById("idBusquedaNombreHerramienta").value.length > 0) {
+        contador = 0
         //recorro el array y si algún string CONTIENE el texto, lo muestro
         for (i = 0; i < arrayOpciones.length; i++) {
             if (arrayOpciones[i].includes(document.getElementById("idBusquedaNombreHerramienta").value)) {
@@ -35,6 +36,7 @@ function filtra() {
                 elementoLista.appendChild(parrafoLista);
                 //atributos
                 parrafoLista.setAttribute("class", "parrafoElementoLista");
+                anadirClaseElementos(parrafoLista);
                 elementoLista.setAttribute("class", "elementoLista");
                 //añadir a la ul
                 let ul = document.getElementById("mostrarBloqueResultados");
@@ -42,8 +44,21 @@ function filtra() {
             }
         }
     }
+
 }
 
+/*Al desplegar la lista de opciones necesitamos que los elementos pares e impares
+tengan atributos distintos para diferenciarse mejor. Esta funcion añade dos clases
+en función de si son pares o impares*/
+function anadirClaseElementos(elemento) {
+    contador++;
+    if (contador % 2 == 0) {
+        elemento.setAttribute("class", "parrafoElementoListaPar");
+    } else {
+        elemento.setAttribute("class", "parrafoElementoListaImpar");
+    }
+
+}
 
 function eliminarElementosBusquedaPrevia(elementosLista) {
 
--- prueba/crearManual.php
+++ prueba/crearManual.php
@@ -11,19 +11,22 @@
     <section>
         <div id="textoAmarillo">
             <img src="Imagenes/crearManual.PNG" alt="Imagen crear">
-            <p> <span id="crearManual">Crear manual:</span> Introduce el nombre de la máquina-herramienta reparada</p>
+            <p>Crear manual</p>
         </div>
         <button id="botonVolver"><a href="../gestionManuales/gestionManuales.php">Volver<span> a gestión de manuales</span></a></button>
     </section>
-    <main id="contenedor">
-        <span id="spanNombresHerramientas"> </span>
-        <form id="formulario">
-            <input id="idBusquedaNombreHerramienta" type="text" name="herramienta" placeholder="Nombre de la herramienta reparada">
-        </form>
-        <ul id="mostrarBloqueResultados">
-
-        </ul>
+    <main>
+        <span id="spanNombresHerramientas">Introduce el nombre de la máquina-herramienta</span>
+        <div id="contenedorBuscarNomHerramienta">
+            <form id="formulario">
+                <input id="idBusquedaNombreHerramienta" type="text" name="herramienta" placeholder="Nombre de la herramienta reparada">
+            </form>
+            <ul id="mostrarBloqueResultados">
+            </ul>
+        </div>
+        <button id="siguentePaso"><a href="">Siguiente paso</a></button>
     </main>
+
 </body>
 
 </html>
