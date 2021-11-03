fd64496292e598053381e4260a6a067cdb3f7827
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2021-11-03 08:31:12 +0100

    (diseño botones)

--- Inicio de sesion/Login.php
+++ Inicio de sesion/Login.php
@@ -13,6 +13,7 @@
 <body>
         <main class="iniciosesion">
                 <div class="logo">
+                        <!--El onclick se utiliza para que al clicar el logo te envie a la pantalla de inicio--->
                         <img class="imagen" src="../Fotos/Fix Point logo.PNG" onclick="window.location='../Inicio de sesion/Login.php';"><br>
                         <label>Inicie sesion para acceder a contenido privado</label>
                 </div><br><br>
@@ -22,10 +23,11 @@
                         <form class="inicio" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" action="LeerBDInicioSesion.php">
                                 <input type="text" name="NombredeUsuario" placeholder="Nombre de usuario"><br><br>
                                 <input type="text" name="Contraseña" placeholder="Contraseña"><br><br>
+                                <!--Si hay un error el mensage aparecera en el span recogido  desde el php ddonde esta el textode error--->
                                 <span><?php echo $error; ?></span>
                                 <span><?php echo $errorinicio; ?></span><br>
-                                <input type="submit" class="botoninicio" name="InicioSesion" value="Inicio de sesion"><br><br>
-                                <button class="crearusuatio">Crear nueva cuenta</button>
+                                <button class="botoninicio">Inicio de sesion</button><br><br>
+                                <button class="crearusuario">Crear nueva cuenta</button>
                         </form>
                 </fieldset>
 
