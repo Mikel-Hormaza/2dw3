3f7d18ad25edd52bd56c41bca396e1dd1cdc9225
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-07 18:32:43 +0100

    (commit on 'Laura')

--- creacionManual/1buscarHerramienta/validarNombreHerramienta.php
+++ creacionManual/1buscarHerramienta/validarNombreHerramienta.php
@@ -6,6 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if(!empty($_POST["herramienta"])){
         $_SESSION['nombreHerramientaSeleccionada']=validarDatos($_POST["herramienta"]);
         echo "herramienta seleccionada: ".$_SESSION['nombreHerramientaSeleccionada'];
+    }else{
+        echo "error";
     }
 }
 
