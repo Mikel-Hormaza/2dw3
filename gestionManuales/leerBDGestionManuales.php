63ad0b3d731a13b5c91e456c76b6200c374d78b8
lauraspe <73583118+lauraspe@users.noreply.github.com>
2021-11-09 14:21:39 +0100

    d

--- gestionManuales/leerBDGestionManuales.php
+++ gestionManuales/leerBDGestionManuales.php
@@ -4,15 +4,17 @@ session_start();
 $servidor  = "localhost";
 $usuario = "root";
 $password = "";
-
+$maxLimit = 8;
+$primeraVariableLimit;
 /* la primera vez que la página se carga, la primera variable vale cero*/
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $primeraVariableLimit = $_SESSION['primeraVariableLimit'];
-}else{
-    $primeraVariableLimit =0;
-}
 
-$maxLimit = 8;
+}else{
+    $primeraVariableLimit=0;
+    $codigoDelUltimoManualDeLaTabla;
+$codigoDelUltimoManualDeLaTablaMostrado;
+}
 
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=fixpoint", $usuario, $password);
@@ -39,6 +41,13 @@ try {
     echo $sqlManuales . "<br>" . $e->getMessage();
 }
 
-setcookie("codigoDelUltimoManualDeLaTablaMostrado",end($datosManuales)["codManual"]);
-setcookie("codigoDelUltimoManualDeLaTabla",end($datoNumTotalManuales)["codManual"]);
+setcookie("codigoDelUltimoManualDeLaTablaMostrado",end($datosManuales)["codManual"],time() + 3600, "/");
+setcookie("codigoDelUltimoManualDeLaTabla",end($datoNumTotalManuales)["codManual"],time() + 3600, "/");
 
+
+$codigoDelUltimoManualDeLaTabla=(intval($_COOKIE['codigoDelUltimoManualDeLaTabla']));
+$codigoDelUltimoManualDeLaTablaMostrado=$_COOKIE['codigoDelUltimoManualDeLaTablaMostrado'];
+
+$_SESSION['primeraVariableLimit']=3;
+require_once "leerBDGestionManuales.php";
+?>
