4f4e5aca8c2a68862c318beb8188a9605564e278
Herrera04 <aitorherreraalves@gmail.com>
2021-11-11 12:50:21 +0100

    (confirmar en 'Aitor')

--- Creacion de Herramientas/CreaciondeHerramietas.php
+++ Creacion de Herramientas/CreaciondeHerramietas.php
@@ -6,22 +6,23 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="icon" type="image/png" href="../Fotos/Icono.ico">
-    <link rel="stylesheet" type="text/css" href="CH.css">
+    <link rel="stylesheet" type="text/css" href="CreacionHerramientas.css">
 
 
 <body>
     <?php require_once '../Header/Header.php'?>
-    <!--button id="botonVolver"><a href="">Volver<span> al inicio</span></a></button-->
+    <button id="botonVolver"><a href="">Volver<span> al inicio</span></a></button>
         <div class="orden">
             <fieldset>
                 <form action="" class="formula">
                     <input type="text" name="izena" class="centrar" placeholder="Nombre"><br>
                     <select name="categoria"class="centrar" placeholder="Categoria">
+                        <option value="C" class="Categoria">Categoria</option>
                         <option value="MH">maquina-herramienta</option>
                         <option value="E">electronica</option>
                         <option value="HT">herramienta taller</option>
                     </select>
-                    <label for="Imagen" class="label-img">Imagen:</label>
+                    <label for="Imagen" class="label-img">Imagen</label>
                     <input type="file" class="br" name="picture">
                 </form>
             </fieldset>
